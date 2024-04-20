2de51814f4cb23c2aa90a8a2f30e508796ce6e95
Albert0nk <153425320+Albert0nk@users.noreply.github.com>
2024-04-20 10:34:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there are elements matching the selector and if the requeriments
+    if (elements.length > instance) {
+        // Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    scrollToElement('.sub-header');
+})
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+})
